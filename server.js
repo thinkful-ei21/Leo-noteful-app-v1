6decb2c622f6a4a8a87bf5314e475242bff224e5
Leo Veres <leoveres@gmail.com>
2018-05-21 15:47:13 -0400

    added search

--- server.js
+++ server.js
@@ -7,11 +7,30 @@ const data = require('./db/notes');
 
 const app = express();
 
+
+
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-  res.json(data);
+
+
+// app.get('/api/notes', (req, res) => {
+//   res.json(data);
+// });
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const list = data.find(item=> item.id === Number(id));
+  res.json(list);
 });
+
+app.get('/api/notes',(req, res) => {
+  const searchTerm=  req.query.searchTerm;
+  const list = data.filter(function(item){
+    return item.title.includes(searchTerm);
+  });
+  res.json(list);
+});
+
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
