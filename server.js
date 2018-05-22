92cd98992f7ed783635682396234fab0cafc7c34
Leo Veres <leoveres@gmail.com>
2018-05-22 16:55:48 -0400

    version 2

--- server.js
+++ server.js
@@ -4,35 +4,86 @@
 const express = require('express');
 
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 
 const app = express();
 
+const {PORT} = require('./config');
+
+const logger = require('./middleware/logger');
 
 
+app.use(logger);
 app.use(express.static('public'));
+app.use(express.json());
 
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
 
-
-// app.get('/api/notes', (req, res) => {
-//   res.json(data);
-// });
-
-app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-  const list = data.find(item=> item.id === Number(id));
-  res.json(list);
-});
-
-app.get('/api/notes',(req, res) => {
-  const searchTerm=  req.query.searchTerm;
-  const list = data.filter(function(item){
-    return item.title.includes(searchTerm);
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); 
+    }
+    res.json(list); 
   });
-  res.json(list);
+});
+
+app.get('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err); 
+    }
+    if(item){
+      res.json(item); 
+    }
+    else{
+      next();
+    }
+  });
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 
-app.listen(8080, function () {
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
