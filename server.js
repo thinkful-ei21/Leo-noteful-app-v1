1be418991946e7cabdedab6c6049877185389dfd
Leo Veres <leoveres@gmail.com>
2018-05-23 14:51:40 -0400

    restful routers

--- server.js
+++ server.js
@@ -3,70 +3,19 @@
 
 const express = require('express');
 
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
-
 const app = express();
 
 const {PORT} = require('./config');
 
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
+const notesRouter = require('./routes/notes.router');
 
-app.use(logger);
+app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
-
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); 
-    }
-    res.json(list); 
-  });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err); 
-    }
-    if(item){
-      res.json(item); 
-    }
-    else{
-      next();
-    }
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
+app.use('/api', notesRouter);
 
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
